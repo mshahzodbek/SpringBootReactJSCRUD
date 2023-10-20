ea4a168b8238a90682014986af948aa6b39bf654
mshahzodbek <mshahzodbek0903@gmail.com>
2023-10-20 07:25:11 +0500

    created project

---
+++ src/main/java/com/example/springbootreactjsfullstackcrud/SpringBootReactJsFullStackCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactjsfullstackcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactJsFullStackCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactJsFullStackCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootreactjsfullstackcrud/SpringBootReactJsFullStackCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactjsfullstackcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactJsFullStackCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
